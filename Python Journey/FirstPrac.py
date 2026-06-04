6732d8fa5df5851249730ceb731ac41730dd0f37
Bernard Aurellano <allenbernardrocamora.aurellano25@bicol-u.edu.ph>
2026-06-04 23:56:55 +0800

    First practice using python

---
+++ Python Journey/FirstPrac.py
@@ -0,0 +1,8 @@
+name = input("Enter yout name: ")
+age = int(input("Enter your age: "))
+FavSub = input("Enter your favorite subject: ")
+
+print("----- STUDENT INFO -----")
+print("Name: ", name)
+print("Age: ", age)
+print("Favorite Subject: ", FavSub)
