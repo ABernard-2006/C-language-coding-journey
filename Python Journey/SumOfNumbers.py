03ef71ae1453ba0680776682d5b86ec318b0641a
Bernard Aurellano <allenbernardrocamora.aurellano25@bicol-u.edu.ph>
2026-06-09 21:32:39 +0800

    Another python practice

---
+++ Python Journey/SumOfNumbers.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number: "))
+total = 0
+x = 1
+
+while x<=num:
+    total += x
+    x+=1
+
+print()
+print(f"The sum from 1 to {num} is {total}.")
--- Python Journey/experiment.py
+++ Python Journey/experiment.py
@@ -2,7 +2,7 @@ print("Hello World!")
 print("This is my 3rd day learning Python Syntax")
 
 print("""
-    multi
-    line
+    multi\n
+    line\n
     print
       """)
